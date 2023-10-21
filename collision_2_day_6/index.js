96a0e15ba71360e0869306c41c96641d256a033c
Anurag-Dhurwey <anuragdhurwey9211@gmail.com>
2023-10-21 19:27:55 +0530

    collison detection gone compleceted

--- collision_2_day_6/index.js
+++ collision_2_day_6/index.js
@@ -26,16 +26,17 @@ document.addEventListener("mousemove", function (e) {
 // });
 
 function getDistance(x1, y1, x2, y2) {
-  console.log("dis");
+  // console.log("dis");
   return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
 
-function obj(X, Y, R) {
+function obj(X, dx, Y, dy, R) {
   this.x = X;
   this.y = Y;
   this.r = R;
   this.color = "black";
-
+  this.dx = dx;
+  this.dy = dy;
   this.draw = function () {
     C.beginPath();
     C.fillStyle = this.color;
@@ -44,16 +45,44 @@ function obj(X, Y, R) {
     C.closePath();
   };
 
-  this.update = function () {
+  this.update = function (circles) {
+    if (
+      Math.floor(this.x) == Math.floor(this.r) ||
+      Math.floor(this.x + this.r) == Math.floor(canvas.width)
+    ) {
+      this.dx = -this.dx;
+    }
+    if (
+      Math.floor(this.y) == Math.floor(this.r) ||
+      Math.floor(this.y + this.r) == Math.floor(canvas.height)
+    ) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
     this.draw();
+
+
+  //   for (let i = 0; i < circles.length; i++) {
+  //     if (this == circles[i]) continue;
+  //     if (
+  //       getDistance(circles[i].x, circles[i].y, this.x, this.y) -
+  //         (circles[i].r + this.r) <
+  //       0
+  //     ) {
+
+  //     } 
+  //   }
   };
+
+
 }
 
 let circles = [];
 
 function getX_Y(R1) {
-  let x = R1 + Math.random() * (canvas.width - R1);
-  let y = R1 + Math.random() * (canvas.height - R1);
+  let x = Math.random() * (canvas.width - R1) + R1;
+  let y = Math.random() * (canvas.height - R1) + R1;
   for (let i = 0; i < circles.length; i++) {
     if (
       getDistance(circles[i].x, circles[i].y, x, y) - (circles[i].r + R1) <
@@ -61,23 +90,25 @@ function getX_Y(R1) {
     ) {
       console.log(x, y);
       i = -1;
-      x = R1 + Math.random() * (canvas.width - R1);
-      y = R1 + Math.random() * (canvas.height - R1);
+      x = Math.random() * (canvas.width - R1) + R1;
+      y = Math.random() * (canvas.height - R1) + R1;
     }
   }
   return { x, y };
 }
 
-for (let i = 0; i < 25; i++) {
+for (let i = 0; i < 15; i++) {
   const R1 = 0.2 + Math.random() * 40;
+  const dx = Math.random() - 0.5;
+  const dy = Math.random() - 0.5;
   const { x, y } = getX_Y(R1);
-  const cir = new obj(x, y, R1);
+  const cir = new obj(x, dx, y, dy, R1);
   circles.push(cir);
 }
 
 function animate() {
   C.clearRect(0, 0, width, height);
-  circles.forEach((cir) => cir.update());
+  circles.forEach((cir) => cir.update(circles));
   requestAnimationFrame(animate);
 }
 
@@ -89,3 +120,4 @@ if (C) {
 
   document.getElementById("error").appendChild(div);
 }
+
